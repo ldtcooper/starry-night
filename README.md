dbcdf5935cec24a13be2860c036c4ae1bf2836e1
Logan Cooper <ldtcoop@gmail.com>
2018-10-14 12:48:56 -0700

    Set up containers for sky and buildings

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # README
+
+## Getting it running
+Run `sass --watch styles.scss:styles.css` to compile the SCSS to CSS
---
+++ starry-night.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  const sky = document.getElementById('sky');
+  const buildingContainer = document.getElementById('building-container');
+
+  const skyDimensions = sky.getBoundingClientRect();
+  const buildingContainerDimensions = buildingContainer.getBoundingClientRect();
+});
